ea29b0e3ab1cb54db0b567245cd71679004bca93
ConnorUsaty <connorusaty@gmail.com>
2024-09-01 19:07:32 -0400

    Implement drawSolution()

--- imageProcessing/imageprocessing_helpers.py
+++ imageProcessing/imageprocessing_helpers.py
@@ -2,11 +2,12 @@ import cv2
 import numpy as np
 import operator
 import math
+from typing import Final
 
 
 # Constants
-height = 450
-width = 450
+HEIGHT: Final[int] = 450
+WIDTH: Final[int] = 450
 
 
 # STEP 1: Preprocess image
@@ -17,11 +18,13 @@ def preprocess(img):
 
     return imgInv
 
+
 # STEP 2: Find contours
 def findContours(img):
     # Find external contours (i.e. grid outline but not individual boxes), no hierarchy, simple approximation
     contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
+
 
 def getCorners(polygon, limit_fn, compare_fn):
     # limit_fn is the min or max function
@@ -60,31 +63,30 @@ def getSudokuGridCorners(contours):
             bot_left = getCorners(contour, min, np.subtract)  # has smallest (x - y) value
             bot_right = getCorners(contour, max, np.add)  # has largest (x + y) value
 
-            print(f"{top_left = }, {top_right = }, {bot_left = }, {bot_right = }")
-
             # Check if the sides are approximately equal
             topSide = distance(top_left, top_right)
             leftSide = distance(top_left, bot_left)
             rightSide = distance(top_right, bot_right)
             bottomSide = distance(bot_left, bot_right)
 
-            print(f"{topSide = }, {leftSide = }, {rightSide = }, {bottomSide = }")
-
             # Check if opposite sides are approximately equal -> Some sudoku images may not be perfect squares (Some may even be rectangles)
-            if abs(topSide - bottomSide) < 10 and abs(leftSide - rightSide) < 10:
-                gridCorners = [top_left, top_right, bot_right, bot_left]
-                break
+            if ((abs(topSide - bottomSide) < 10) and (abs(leftSide - rightSide) < 10)
+                 and (topSide / leftSide) > 0.6 and (topSide / leftSide) < 1.4): # Check if it's a square with large tolerance but not large enough to get non-grid rectangles
+                    gridCorners = [top_left, top_right, bot_right, bot_left]
+                    break
 
     return gridCorners
+
 
 # STEP 4: Crop to grid only
 def cropToGridOnly(img, corners):
     pts1 = np.float32(corners)
-    pts2 = np.float32([[0,0], [width,0], [width,height], [0,height]])
+    pts2 = np.float32([[0,0], [WIDTH,0], [WIDTH,HEIGHT], [0,HEIGHT]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    imgWarp = cv2.warpPerspective(img, matrix, (width, height))
+    imgWarp = cv2.warpPerspective(img, matrix, (WIDTH, HEIGHT))
 
     return imgWarp
+
 
 # STEP 5: Get grid boxes -> Extract individual boxes / digits
 def getGridBoxes(img):
@@ -112,3 +114,28 @@ def getPredictions(boxes, model):
         grid.append(classIndex[0])
             
     return grid
+
+
+# STEP 7: Draw solution on image
+def drawSolution(warped_img, solved_puzzle, squares_processed):
+    warped_img = cv2.bitwise_not(warped_img, 0) # Turn image back to black background, white text
+
+    width = warped_img.shape[0] // 9
+    img_w_text = warped_img
+
+    index = 0
+    for j in range(9):
+        for i in range(9):
+            if squares_processed[index] == 0: # If square was empty
+                p1 = (i * width, j * width)  # Top left corner of a bounding box
+                p2 = ((i + 1) * width, (j + 1) * width)  # Bottom right corner of bounding box
+
+                center = ((p1[0] + p2[0]) // 2, (p1[1] + p2[1]) // 2)
+                text_size, _ = cv2.getTextSize(str(solved_puzzle[index]), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 4)
+                text_origin = (center[0] - text_size[0] // 2, center[1] + text_size[1] // 2)
+
+                cv2.putText(warped_img, str(solved_puzzle[index]),
+                            text_origin, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            index += 1
+
+    return img_w_text
