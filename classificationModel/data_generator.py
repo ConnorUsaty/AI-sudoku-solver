46d633891676110e4c14913517ef165af3ae285b
ConnorUsaty <connorusaty@gmail.com>
2024-09-01 15:58:49 -0400

    Upgraded data generation script
    Now has randomized font sizes, borders, and offsets from the center of the image to better simulate an actual sudoku grid.

--- classificationModel/data_generator.py
+++ classificationModel/data_generator.py
@@ -1,21 +1,42 @@
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont, ImageOps, ImageChops
 from tqdm import tqdm
+import time
+import random
 
 
-def create_text_image(text, font_path, font_size=32, rotation=0, noise_level=0):
+def create_text_image(text, font_path, font_size=24, rotation=0, noise_level=0):
     # Generate an image with the specified text and font
-    font = ImageFont.truetype(font_path, font_size)
-    image = Image.new('L', (28, 28), color=0)  # 'L' mode for grayscale, black background
-    draw = ImageDraw.Draw(image)
-    bbox = draw.textbbox((0, 0), text, font=font)
-    width, height = bbox[2] - bbox[0], bbox[3] - bbox[1]
-    draw.text(((28 - width) / 2, (28 - height) / 2 - 6), text, font=font, fill=255)  # White text
+    if text != '0':
+        font = ImageFont.truetype(font_path, font_size)
+        image = Image.new('L', (28, 28), color=0)  # 'L' mode for grayscale, black background
+        draw = ImageDraw.Draw(image)
+        bbox = draw.textbbox((0, 0), text, font=font)
+        width, height = bbox[2] - bbox[0], bbox[3] - bbox[1]
+        dw = random.randint(-5, 5)  # Random horizontal offset
+        dh = random.randint(-5, 5)  # Random vertical offset
+        draw.text(((28 - width) / 2 - dw, (28 - height) / 2 - 6 - dh), text, font=font, fill=255)  # White text
+    else:
+        image = Image.new('L', (28, 28), color=0)
 
     # Apply rotation
     if rotation != 0:
         image = image.rotate(rotation, expand=1, fillcolor=0)
         image = ImageOps.fit(image, (28, 28), method=0, bleed=0.0, centering=(0.5, 0.5))
+
+    # Randomly add white borders to simulate sudoku grid lines on edge of image
+    border_sides = random.sample(['top', 'bottom', 'left', 'right'], random.randint(1, 4))
+    border_width = random.randint(1, 5)  # Random border width between 1 and 4 pixels
+    image = np.array(image)
+    if 'top' in border_sides:
+        image[:border_width, :] = 255
+    if 'bottom' in border_sides:
+        image[-border_width:, :] = 255
+    if 'left' in border_sides:
+        image[:, :border_width] = 255
+    if 'right' in border_sides:
+        image[:, -border_width:] = 255
+    image = Image.fromarray(image)
     
     # Apply noise
     if noise_level != 0:
@@ -35,29 +56,33 @@ def main():
         'georgia.ttf',
         'comic.ttf',
     ]
-    # Use a large variety of different angles and noise levels for training data
-    angles = [i for i in range(-15, 16)]
-    noise_levels = [i for i in range(0, 16)]
+    # Use a large variety of font sizes, different angles, and noise levels for training data
+    font_sizes = [i for i in range(18, 33, 2)]
+    angles = [i for i in range(-18, 19, 2)]
+    noise_levels = [i for i in range(0, 25)]
 
+    images = 0
+    start_time = time.time()
     # Generate and save images for numbers 1-9 in each font with specified angles and noise levels
-    # Don't need to generate images for 0 since it will be used to represent empty cells
+    # Will generate blank images for 0
     for font in popular_fonts:
-        for i in tqdm(range(1, 10), desc=f'Generating images for {font}'):
-            for angle in angles:
-                for noise_level in noise_levels:
-                    img = create_text_image(str(i), font, rotation=angle, noise_level=noise_level)
+        for i in tqdm(range(0, 10), desc=f'Generating images for {font.split(".")[0]}'):
+            for font_size in font_sizes:
+                for angle in angles:
+                    for noise_level in noise_levels:
+                        img = create_text_image(str(i), font, font_size=font_size, rotation=angle, noise_level=noise_level)
 
-                    # Save image with appropriate name (No dashes allowed in file names)
-                    if angle < 0:
-                        angle_str = "neg" + str(abs(angle))
-                    else:
-                        angle_str = str(angle)
-                    if noise_level < 0:
-                        noise_level_str = "neg" + str(abs(noise_level))
-                    else:
-                        noise_level_str = str(noise_level)
+                        # Save image with appropriate name (No dashes allowed in file names)
+                        if angle < 0:
+                            angle_str = "neg" + str(abs(angle))
+                        else:
+                            angle_str = str(angle)
 
-                    img.save(f'classificationModel/trainingImages/{font.split(".")[0]}_text_{i}_angle_{angle_str}_noise_{noise_level_str}.png')
+                        img.save(f'classificationModel/trainingImages/{font.split(".")[0]}_text_{i}_size_{font_size}_angle_{angle_str}_noise_{noise_level}.png')
+                        images += 1
+
+    print(f'\nGenerated {images} images')
+    print(f'Elapsed time: {time.time() - start_time}')
 
 
 if __name__ == '__main__':
